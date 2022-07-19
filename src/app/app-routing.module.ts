bb40952cd7db7f9b3c62cbc5a25c49714db8deea
Dima Savitski <dzmitry.savitski@innowise-group.com>
2022-07-19 17:12:51 +0300

    added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 import { HomeComponent } from './home/home/home.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'customers', component: CustomerListComponent}
+  { 
+    path: 'customers', component: CustomerListComponent,
+    canActivate: [AuthGuard],
+  }
 ];
 
 @NgModule({
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
@@ -8,14 +8,17 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(auth: AuthService){
-
+  constructor(private auth: AuthService, private router: Router){
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if (this.auth.isAuthenticated()) {
+        return true;
+      }
+      this.auth.logout();
+      this.router.navigate(['']);
+      return false;
   }
-  
 }
